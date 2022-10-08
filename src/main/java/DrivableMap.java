43f0f6900ecccd3367089727844323a56720b9e1
arya <sid19arya@gmail.com>
2022-10-08 17:37:42 -0400

    Tasks completed

--- src/main/java/DrivableMap.java
+++ src/main/java/DrivableMap.java
@@ -7,6 +7,7 @@
  * created the constructor for you already.
  */
 
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -27,7 +28,14 @@ class DrivableMap {
      *       in drivable_map, then add the pair to drivable_map.
      *       Return true if the Drivable was added to drivable_map.
      */
-
+    public boolean addDrivable (String id, Drivable object){
+        if (this.drivable_map.containsKey(id)){
+            return false;
+        } else {
+            this.drivable_map.put(id, object);
+            return true;
+        }
+    }
 
 
 
@@ -37,7 +45,14 @@ class DrivableMap {
      * You may want to use drivable_map.keys() or drivable_map.values() to
      * iterate through drivable_map.
      */
-
+    public boolean hasFasterThan (int givenMaxSpeed){
+        for (Drivable object : drivable_map.values()) {
+            if (object.getMaxSpeed() >= givenMaxSpeed) {
+                return true;
+            }
+        }
+        return false;
+    }
 
 
 
@@ -46,7 +61,15 @@ class DrivableMap {
      *       returns a List containing all of the Tradable items in
      *       drivable_map.
      */
-
+    public List<Tradable> getTradable() {
+        ArrayList<Tradable> tradableItems = new ArrayList<Tradable>();
+        for (Drivable item: drivable_map.values()){
+            if (item instanceof Tradable){
+                tradableItems.add((Tradable) item);
+            }
+        }
+        return tradableItems;
+    }
 
 
     
